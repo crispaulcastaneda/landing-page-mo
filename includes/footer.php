b96534c341bb22ce29558be248b8992107b0bd14
crispaulcastaneda <cristianpaul_c@yahoo.com>
2025-02-05 00:18:49 +0800

    MO-BANNER-V1.0 UPDATE

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="assets/js/swiper.js"></script>
+<script src="assets/js/script.js"></script>
+</body>
+
+</html>
