7f3c49a12c8fffda11ed7eee3b5cc0d0dbc1d479
crispaulcastaneda <cristianpaul_c@yahoo.com>
2025-02-05 23:50:37 +0800

    MAIN BRANCH CHECKER

---
+++ public/assets/js/swiper.js
@@ -0,0 +1,7 @@
+let SWIPER = new Swiper(".BAN_SWIPER", {
+  autoplay: {
+    delay: 3000, // 3000ms = 3s
+    disableOnScroll: false, // keeps on autoplaying even if you scroll
+  },
+  loop: true, // infinite loop
+});
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php include_once '../includes/head.php'; ?>
+
+<main>
+
+  <?php include_once '../includes/content-banner.php' ?>
+
+</main>
+
+<?php include_once '../includes/footer.php'; ?>
