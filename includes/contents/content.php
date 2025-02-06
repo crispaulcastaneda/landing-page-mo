d8f0aa501acc9a9f990ea4450ddb00250e6bf89d
crispaulcastaneda <cristianpaul_c@yahoo.com>
2025-02-06 22:29:01 +0800

    02 MC Tiles V1.01 Added contents and responsive - 70% completion

--- includes/contents/content.php
+++ includes/contents/content.php
@@ -1,7 +1,10 @@
+<!-- HEAD -->
 <?php
 $language = "en-US";
 $title = "Project | Maria Ozawa Microsite";
-
+?>
+<!-- BANNER -->
+<?php
 $HEADER1 = "Lorem ipsum dolor sit";
 $HEADER2 = "Excepteur sint occaecat";
 $SUBHEADER = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore";
@@ -17,4 +20,31 @@ $BANNERLINKTXT2 = "Join Now";
 $BANNER1LINKURLPLAY = "#";
 $BANNERLINKTXT3 = "PLAY NOW";
 $BANNER2LINKURL = "#";
+?>
+<!-- MC CONTENT -->
+<?php
+$MCLOGO = "assets/images/icons/02-mc-logo.svg";
+$MCLOGOALT = "Maria's Choice";
+
+$MCCARD1_ALT = "MO Choice Card 1";
+$MCCARD2_ALT = "MO Choice Card 2";
+$MCCARD3_ALT = "MO Choice Card 3";
+
+$MCCARDSTARFULL = "assets/images/misc/02-mc-tile-star-full.png";
+$MCCARDSTARFULLTXT = "STARS";
+
+$MCCARDURL1 = "#";
+$MCCARDTXT1 = "JOIN NOW";
+$MCCARDURL2 = "#";
+$MCCARDTXT2 = "JOIN NOW";
+$MCCARDURL3 = "#";
+$MCCARDTXT3 = "JOIN NOW";
+
+$MCCONTENTABOUT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse condimentum vestibulum interdum. Nunc in augue nisi. Suspendisse potenti. Vestibulum non dictum nisi. Sed et erat finibus, pulvinar neque tincidunt, vestibulum purus. Proin ultrices cursus quam. Suspendisse condimentum vestibulum interdum. Nunc in augue nisi.";
+
+$TILEIMG = "assets/images/tile/02-mc-tile.webp";
+$TILEIMGALT = "TILE IMAGE";
+$MCTILEURL = "#";
+$GAMENAME = "GAME NAME";
+$RATESTAR = "assets/images/misc/02-mc-tile-star.png";
 ?>
--- public/assets/js/swiper.js
+++ public/assets/js/swiper.js
@@ -1,9 +1,39 @@
 "use strict";
 
-let SWIPER = new Swiper(".BAN_SWIPER", {
-  autoplay: {
-    delay: 30000, // 3000ms = 3s
-    disableOnScroll: false, // keeps on autoplaying even if you scroll
-  },
-  loop: true, // infinite loop
-});
+const swiperContainer = () => {
+  // BANNER
+  let BAN_SWIPER = new Swiper(".BAN_SWIPER", {
+    autoplay: {
+      delay: 3000, // 3000ms = 3s
+      disableOnScroll: false, // keeps on autoplaying even if you scroll
+    },
+    loop: true, // infinite loop
+  });
+
+  let MC_SWIPER = new Swiper(".MCSWIPER", {
+    slidesPerView: 2,
+    spaceBetween: 16,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      1024: { slidesPerView: "auto", spaceBetween: 16 },
+      768: { slidesPerView: "auto", spaceBetween: 8 },
+      375: { slidesPerView: "auto", spaceBetween: 8 },
+    },
+  });
+  let MC_TILE = new Swiper(".MCTILES", {
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    freeMode: true,
+    breakpoints: {
+      1024: { slidesPerView: "auto", spaceBetween: 12 },
+      768: { slidesPerView: "auto", spaceBetween: 8 },
+      375: { slidesPerView: "auto", spaceBetween: 6 },
+    },
+  });
+};
+
+swiperContainer();
